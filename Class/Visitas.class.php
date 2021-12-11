ecfb56b03107bcff2754c90663ff611668da5f13
LuckyLewis <afortunadoluis@gmail.com>
2021-12-11 15:30:16 +0100

    Añadido el método locateIP()

--- Class/Visitas.class.php
+++ Class/Visitas.class.php
@@ -12,12 +12,12 @@ class Visitas
     private $so;
     private $requestTime;
 
-    public function __construct($username, $ip,$fecha, $serverName, $browser, $so, $requestTime)
+    public function __construct($username, $ip, $fecha, $serverName, $browser, $so, $requestTime)
     {
         $this->username = $username;
         $this->serverName = $serverName;
         $this->ip = $ip;
-        $this->fecha=$fecha;
+        $this->fecha = $fecha;
         $this->browser = $browser;
         $this->so = $so;
         $this->requestTime = $requestTime;
@@ -68,7 +68,8 @@ class Visitas
      * El método devuelve la ip del usuario. Si, por lo que sea, no es capaz
      *  de averiguarla, devolverá la string "IP desconocida".
      */
-    public static function guessIP() {
+    public static function guessIP()
+    {
         $ip = "IP desconocida";
         if (getenv('HTTP_CLIENT_IP')) {
             $ip = getenv('HTTP_CLIENT_IP');
@@ -85,5 +86,25 @@ class Visitas
         }
 
         return $ip;
+    }
+
+    /**
+     * Pasándole una dirección IP como parámetro, el método nos devolverá
+     * una string con la ubicación de la misma. En caso de no conocerla,
+     * devolverá "Ubicación desconocida".
+     * 
+     * @param string $ip La IP que se desea localizar.
+     * @return string La localización de la IP en formato "Ciudad, Región".
+     */
+    public static function locateIP($ip)
+    {
+        $dataArray = json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+
+        var_dump($dataArray);
+        if ($dataArray["geoplugin_city"] != null && $dataArray["geoplugin_region"] != null) {
+            return $dataArray["geoplugin_city"] . ", " . $dataArray["geoplugin_region"];
+        } else {
+            return "Ubicación desconocida";
+        }
     }
 }
