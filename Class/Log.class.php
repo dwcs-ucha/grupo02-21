ff56973ef6d7265016695f9e6f3a02ff04a8d827
LuckyLewis <afortunadoluis@gmail.com>
2021-12-11 10:20:29 +0100

    Actualización de la clase Log

--- Class/Log.class.php
+++ Class/Log.class.php
@@ -1,23 +1,34 @@
 <?php
+/**
+ * Clase validaciones.class.php
+ *
+ * @author luisvi
+ * @email luisvaziza@gmail.com
+ * @fechaDeCreación 18 nov 2021
+ * @últimaModificación 11 dic 2021
+ * @versión v01.03.00
+ */
 class Log {
 
 private string $fecha;
-private string $error;
+//private string $error;
 private string $usuario;
 private string $texto;
 
-public function __construct($texto, $usuario = "invitado", $error = "") {
+//public function __construct($texto, $usuario = "invitado", $error = "") {
+public function __construct($texto, $usuario = "invitado") {
     $fecha = getdate();
     $this->fecha = $fecha['year'] . "." . sprintf("%02d", $fecha['mon']) . "." . sprintf("%02d", $fecha['mday'])
     . "-" . sprintf("%02d", $fecha['hours']) . ":" . sprintf("%02d", $fecha['minutes']) . ":" . sprintf("%02d", $fecha['seconds']);
 
-    $this->error = $error;
+    //$this->error = $error;
     $this->usuario = $usuario;
     $this->texto = $texto;
 }
 
 public function __toString() {
-    return $this->fecha . " " . $this->error . " " . $this->usuario . " - " . $this->texto;
+    //return $this->fecha . " - " . $this->error . " - " . $this->usuario . " - " . $this->texto;
+    return $this->fecha . " - " . strtoupper($this->usuario) . " " . $this->texto;
 }
 }
 ?>
