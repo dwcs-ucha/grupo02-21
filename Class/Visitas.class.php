3c0be939b72f2a36a3e8cc3d5db54e6675002604
LuckyLewis <afortunadoluis@gmail.com>
2021-12-11 11:41:34 +0100

    Añadido el método guessIP() al final de la clase

--- Class/Visitas.class.php
+++ Class/Visitas.class.php
@@ -63,4 +63,27 @@ class Visitas
         $format = [$this->username, $this->ip, $this->fecha, $this->serverName, $this->browser, $this->so, $this->requestTime];
         return $format;
     }
+
+    /**
+     * El método devuelve la ip del usuario. Si, por lo que sea, no es capaz
+     *  de averiguarla, devolverá la string "IP desconocida".
+     */
+    public static function guessIP() {
+        $ip = "IP desconocida";
+        if (getenv('HTTP_CLIENT_IP')) {
+            $ip = getenv('HTTP_CLIENT_IP');
+        } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif (getenv('HTTP_X_FORWARDED')) {
+            $ip = getenv('HTTP_X_FORWARDED');
+        } elseif (getenv('HTTP_X_FORWARDED_FOR')) {
+            $ip = getenv('HTTP_X_FORWARDED_FOR');
+        } elseif (getenv('HTTP_X_FORWARDED')) {
+            $ip = getenv('HTTP_X_FORWARDED');
+        } else {
+            $ip = $_SERVER["REMOTE_ADDR"];
+        }
+
+        return $ip;
+    }
 }
