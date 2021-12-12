f20ed8e694171002981d5e7b06663f5f45783538
ManuRioss <94856088+ManuRioss@users.noreply.github.com>
2021-12-12 22:48:04 +0100

    Cambios calculadora
    Cambios calculadora

--- utiles/Manuel/calculadora.php
+++ utiles/Manuel/calculadora.php
@@ -1,13 +1,11 @@
+<?php session_start(); ?>
 <html>
 	<head>
 
 		    <link rel="stylesheet" href="../../css/custom.css">
-	   	    <?php
-		    	include '../../head.php'; 
-		    ?>
+	   	    <?php include '../../componentes/head.php'; ?>
   
-		<meta charset="utf-8">
-		<title>Calculadora del gasto energético anual de un electrodoméstico</title>
+		<title>Calculadora do gasto enerxético anual dun electrodoméstico</title>
 		<?php
 		/*
 		Versión: 1.0.0 
@@ -21,10 +19,11 @@
 		</style>
 	</head>
 	<body>
-		<?php
-            		include '../../menu.php';
-        	?>
+		<?php include '../../componentes/menu.php'; ?>
+		<?php include_once '../../componentes/cookieAlert.php';?>
 		<div class="fondo">
+		<div class="container py-5">
+		<div class="container border border-5 border border-primary border rounded-3 bg-light ">
 		<form action="calculadora.php" method="post">
 			<fieldset>
 				<?php
@@ -54,18 +53,18 @@
                                                 
                                         }
 				?>
-				<legend>Calculadora de gasto energético</legend>
+				<legend class="text-primary">Calculadora do gasto enerxético anual dun electrodoméstico</legend>
 				<p> 
-					Consumo del dispositivo en vatios <br/>
-					<?php echo $error_vatios; ?><br/>
-                                        <?php echo $error_vatios2; ?><br/>
+					Consumo do dispositivo en vatios
+					<?php echo $error_vatios."<br/>"; ?>
+                                        <?php echo $error_vatios2."<br/>"; ?>
 					<input type="text" name="vatios" value="<?php echo $vatios ?>" >
 				</p>
 				<p>
-					¿Con qué frecuencia se utiliza el dispositivo normalmente?<br/>
-					<?php echo $error_frecuencia; ?><br/>
+					¿Con qué frecuencia utilízase o dispositivo normalmente?<br/>
+					<?php echo $error_frecuencia."<br/>"; ?>
 					<select name="frecuencia">
-						<option value ="" selected>(Seleccionar una opción)</option>
+						<option value ="" selected>(Seleccionar unha opción)</option>
                                                 <option value="A diario" <?php if($frecuencia == "A diario"){$valorFrecuencia=365; echo "selected";}?>>A diario</option>
 						<option value="Cada 2 días" <?php if($frecuencia == "Cada 2 días"){$valorFrecuencia=365/2; echo "selected";}?>>Cada 2 días</option>
                                                 <option value="6 veces a la semana" <?php if($frecuencia == "6 veces a la semana"){$valorFrecuencia=6*52.1429; echo "selected" ;}?>>6 veces a la semana</option>
@@ -82,10 +81,10 @@
 					</select>
 				</p>
 				<p>
-					¿Cuánto utiliza el dispositivo normalmente?<br/>
-					<?php echo $error_tiempo; ?><br/>
+					¿Canto utiliza o dispositivo normalmente?<br/>
+					<?php echo $error_tiempo."<br/>"; ?>
 					<select name="tiempo">
-						<option value ="" selected>(Seleccionar una opción) </option>
+						<option value ="" selected>(Seleccionar unha opción) </option>
                                                 <option value ="30 minutos" <?php if($tiempo == "30 minutos"){$valorTiempo=0.5; echo "selected ";}?>>30 minutos</option>
 						<option value="1 hora" <?php if($tiempo == "1 hora"){$valorTiempo=1; echo "selected"; }?>>1 hora</option>
                                                 <option value="2 horas" <?php if($tiempo == "2 horas"){$valorTiempo=2; echo "selected";}?>>2 horas</option>
@@ -100,9 +99,9 @@
 					</select>
 				</p>
 				<p> 
-					Coste de la electricidad en € por kilovatio hora <br/>
-					<?php echo $error_euros; ?><br/>
-                                        <?php echo $error_euros2; ?><br/>
+					Coste da electricidade en € por kilovatio hora
+					<?php echo $error_euros."<br/>"; ?>
+                                        <?php echo $error_euros2."<br/>"; ?>
 					<input type="text" name="euros" value="<?php echo $euros ?>">
 				</p>
 				<p><button type="submit" style="background-color:#4CFE24" name='submit'>Calcular</button></p>
@@ -114,43 +113,45 @@
                             }
                             ?>
 		</form>
-                <h1>Uso de medidores de corriente</h1>
-                <p>Cuando se trata de ahorrar electricidad, es importante saber qué dispositivo requiere más energía eléctrica. Por lo tanto, debe medir el consumo de energía de cada dispositivo eléctrico con un medidor de corriente. Los más grandes devoradores de energía son la estufa, lavadora, secador de pelo, cafetera, nevera y secadora.</p>
+		</div>
+		<br/>
+                <h2 class="text-primary"> Usando medidores de corrente </h1>
+                <p> Cando se trata de aforrar electricidade, é importante saber que dispositivo require máis enerxía eléctrica. Polo tanto, cómpre medir o consumo de enerxía de cada dispositivo eléctrico cun medidor de corrente. Os maiores consumidores de enerxía son a cociña, a lavadora, o secador de pelo, a cafeteira, a neveira e a secadora. </p>
 
-                <h1>Ahorro de energía con electrodomésticos</h1>
-                <p>En muchos hogares, numerosos aparatos eléctricos facilitan la vida diaria. Hay que pagar un precio. Hemos preparado una serie de consejos para ayudarle a reducir su consumo de energía. Estos consejos por sí solos pueden ahorrarle hasta un 40% en costos de electricidad.</p>
+                <h2 class="text-primary"> Aforro de enerxía con electrodomésticos </h1>
+                <p> En moitas casas, numerosos electrodomésticos facilitan a vida diaria. Hai un prezo que pagar. Preparamos unha serie de consellos para axudarche a reducir o teu consumo de enerxía. Só estes consellos poden aforrarche ata un 40 % nos custos de electricidade. </p>
 
-                <h1>Cuelgue la ropa en lugar de usar una secadora de ropa</h1>
-                <p>Especialmente en verano se puede prescindir de la secadora de ropa y colgar la ropa lavada con una correa – en el jardín, en el balcón o simplemente en su apartamento. Esto le permite reducir sus costes de forma rápida y sencilla. Si no desea prescindir de la secadora, asegúrese de que la lavadora esté ajustada a una alta velocidad durante el centrifugado. Así pues, consume un poco más de electricidad. Pero el secador tiene que funcionar menos y necesita mucha menos energía.</p>
+                <h2 class="text-primary"> Colga a roupa en lugar de usar unha secadora </h1>
+                <p> Especialmente no verán pode prescindir da secadora e colgar a roupa lavada nunha correa: no xardín, no balcón ou simplemente no seu apartamento. Isto permítelle reducir os seus custos de forma rápida e sinxela. Se non queres prescindir da secadora, asegúrate de que a lavadora estea a alta velocidade durante o centrifugado. Polo que consome un pouco máis de electricidade. Pero a secadora ten que funcionar menos e necesita moita menos enerxía. </p>
 
-                <h1>Desconectar completamente el televisor viejo</h1>
-                <p>Un televisor nuevo consume mucha menos electricidad que un televisor antiguo. Esto no significa que usted debe tirar simplemente su TV de trabajo lejos y substituirlo con un nuevo modelo. Sin embargo, compruebe siempre el ajuste de brillo y atorníllelo si es necesario. Debido al consumo en modo de espera, también vale la pena apagar un televisor antiguo.</p>
+                <h2 class="text-primary"> Desconecta completamente o televisor antigo </h1>
+                <p> Un televisor novo usa moita menos electricidade que un televisor vello. Isto non significa que simplemente debas tirar o teu televisor que funciona e substituílo por un modelo novo. Non obstante, comprobe sempre a configuración de brillo e atorníllaa se é necesario. Debido ao consumo en modo de espera, tamén paga a pena apagar un televisor vello. </p>
 
-                <h1>Ajustar el secador de pelo a un nivel bajo</h1>
-                <p>Coloca tu secador de pelo a una temperatura baja o media. Efecto secundario agradable: Protege el cabello, que de lo contrario sería atacado por el aire caliente.</p>
+                <h2 class="text-primary"> Establece o secador de cabelo nunha configuración baixa </h1>
+                <p> Establece o secador de cabelo nunha configuración baixa ou media. Efecto secundario agradable: protexe o cabelo, que doutro xeito sería atacado polo aire quente. </p>
 
-                <h1>Descongelar el congelador o congelador</h1>
-                <p>No hay manera más fácil de ahorrar dinero: descongele su congelador o el congelador cuando se haya formado una capa de escarcha. Después, el aparato se enfría de forma más eficiente y ahorra electricidad.</p>
+                <h2 class="text-primary"> Desconxela o conxelador ou conxelador </h1>
+                <p> Non hai xeito máis sinxelo de aforrar diñeiro: desconxela o teu conxelador ou conxelador cando se forma unha capa de xeadas. O aparello arrefría de forma máis eficiente e aforra electricidade. </p>
 
-                <h1>La tetera es mejor que la estufa</h1>
-                <p>Si tiene una estufa más vieja: Caliente el agua de la pasta en la tetera y viértala en la olla. Para preparar los huevos para el desayuno, es mejor usar una olla de huevos. Requiere menos electricidad que una estufa.</p>
+                <h2 class="text-primary"> A chaleira é mellor que a cociña </h1>
+                <p> Se tes unha cociña máis antiga: quenta a auga da pasta na chaleira e bótaa na pota. Para preparar ovos para o almorzo, é mellor usar unha cociña de ovos. Necesita menos electricidade que unha cociña. </p>
 
-                <h1>Reemplazo de bombillas</h1>
-                <p>Si todavía tiene bombillas viejas, sustitúyalas por bombillas de ahorro de energía o lámparas LED. Por cierto, tanto las lámparas de ahorro energético como las lámparas LED están disponibles en diferentes colores de luz e intensidades de radiación. Si quieres una luz cálida y acogedora, asegúrate de que el valor del paquete sea inferior a 3.300 Kelvin. Para la luz blanca que corresponde a la luz del sol, el valor debe ser significativamente mayor.</p>
+                <h2 class="text-primary"> Substitución da lámpada </h1>
+                <p> Se aínda tes lámpadas antigas, substitúeas por lámpadas de aforro enerxético ou lámpadas LED. Por certo, tanto as lámpadas de aforro enerxético como as lámpadas LED están dispoñibles en diferentes cores de luz e intensidades de radiación. Se queres unha luz cálida e acolledora, asegúrate de que o valor do paquete sexa inferior a 3.300 Kelvin. Para a luz branca que corresponde á luz solar, o valor debería ser significativamente maior. </p>
 
-                <h1>No calentar con electricidad o aire acondicionado</h1>
-                <p>No use un calefactor de ventilador o aire acondicionado para calentar el apartamento, ya que la calefacción con electricidad es muy cara.</p>
+                <h2 class="text-primary"> Non quentar con electricidade ou aire acondicionado </h1>
+                <p> Non uses un ventilador ou aire acondicionado para quentar o apartamento, xa que a calefacción con electricidade é moi cara. </p>
 
-                <h1>Desconectar pequeños calentadores de agua</h1>
-                <p>¿Tiene una pequeña caldera de agua caliente debajo del lavabo en el baño? Luego piense en lavarse las manos con agua fría y cepillarse los dientes. Pequeñas calderas y calentadores de agua instantáneos consumen mucha electricidad.</p>
+                <h2 class="text-primary"> Desconecta os pequenos quentadores de auga </h1>
+                <p> Tes unha pequena caldeira de auga quente debaixo do lavabo do baño? Despois pensa en lavar as mans con auga fría e lavar os dentes. As pequenas caldeiras e os quentadores de auga instantáneos consomen moita electricidade. </p>
 
-                <h1>No precalentar el horno</h1>
-                <p>En el paquete de casi todas las comidas listas para servir, dice que debe precalentar el horno antes de poner la comida. Sin embargo, los fabricantes generalmente recomiendan esto sólo para poder dar un tiempo exacto de preparación. Por regla general, no es necesario precalentar el horno. Esto sólo cuesta electricidad inútil y no hace la comida más deliciosa.</p>
+                <h2 class="text-primary"> Non prequentar o forno </h1>
+                <p> No paquete de case todas as comidas listas para servir, di que prequentar o forno antes de meter a comida. Non obstante, os fabricantes xeralmente recomendan isto só para dar un tempo de preparación preciso. Como regra xeral, non é necesario prequentar o forno. Isto só custa electricidade inútil e non fai que a comida sexa máis deliciosa. </p>
 
-                <h1>Nuevos electrodomésticos? Asegure un bajo consumo de energía!</h1>
-                <p>Con un golpe, el consumo de energía se puede reducir enormemente reemplazando un refrigerador, congelador o lavadora antiguos. Los dispositivos nuevos tienen un menor consumo que los antiguos.</p>
+                <h2 class="text-primary"> Electrodomésticos novos? Asegura un baixo consumo de enerxía! </h1>
+                <p> Cun só golpe, pódese reducir moito o consumo de enerxía substituíndo unha neveira, conxelador ou lavadora antigas. Os novos dispositivos teñen un menor consumo que os antigos. </p>
+	</div>
 
-
-	<div class="fondo">
+	</div>
 	</body>
 </html>
