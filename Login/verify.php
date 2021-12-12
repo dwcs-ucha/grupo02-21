250fa9d8f92e0d38967bf6b150488256596d4055
rdn998 <rdn998@gmail.com>
2021-12-12 20:36:06 +0100

    Mails

--- Login/verify.php
+++ Login/verify.php
@@ -3,6 +3,7 @@
 if (isset($_GET['email']) && !empty($_GET['email']) and isset($_GET['hash']) && !empty($_GET['hash'])) {
     Persona::validate_pw($_GET['email'], $_GET['hash']);
     $user = DAO::getUser('', $_GET['email']);
+    $user->setActive(1);
     DAO::updateUser($user);
 } else {
     // Invalid approach
