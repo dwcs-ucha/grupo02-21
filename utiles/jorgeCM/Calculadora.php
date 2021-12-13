f74a9bea0a0c7b33a5cd008fa24af543a8e71fa3
JorgeCM00 <jorcarmir@gmail.com>
2021-12-13 22:09:45 +0100

    actualizacion en mi calculadora y la doy por terminada

--- utiles/jorgeCM/Calculadora.php
+++ utiles/jorgeCM/Calculadora.php
@@ -5,6 +5,8 @@
  * Last modified: 8/12/2021
  */
 include_once './Class/classCalculadora.php';
+require_once '../../Class/Validacion.class.php';
+require_once '../../Class/Erro.class.php';
  include '../../componentes/head.php'; 
 
 session_start();
@@ -13,6 +15,7 @@ if (isset($_SESSION['user'])) {
 } else {
     header("Location: ../../Login/signIn.php");
 }
+$datosOk=true;
 ?>
 <html>
     <head>
@@ -27,6 +30,12 @@ if (isset($_SESSION['user'])) {
         <div class="container pt-5">
             <h1 class="text-primary">Calculadora eficiencia enerxética</h1>
             <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" name="formulario" id="formulario" style="margin:0;">
+                <?php 
+                $calefaccion="";
+                $ACS="";
+                $refrixeracion="";
+                $superficie="";
+                ?>
                 <fieldset class="fieldset">
                      <div class="row">
                          <div class="col-6">
@@ -34,8 +43,9 @@ if (isset($_SESSION['user'])) {
                 <pre> kWh/m²                        Tipo</pre>
                 <div class="row mb-4">
                     <div class="col-4">
-                <input class="input-group-text" style="width: 100%" type="number" name="calefaccion" value="">
+                <input class="input-group-text" style="width: 100%" type="number" name="calefaccion" value="<?php $calefaccion; ?>">
                     </div>
+                    <?php validaCal(); ?>
                     <div class="col-8">
                 <select name="tipoCalc" class="form-select input-group-text">
                     <option value="Gas" <?php if(isset($_POST['enviar']) && ($_POST['calefaccion']=="Gas")) echo "selected"; ?>>Gas</option>
@@ -48,8 +58,9 @@ if (isset($_SESSION['user'])) {
                 <pre> kWh/m²                        Tipo</pre>
                 <div class="row mb-4">
                     <div class="col-4">
-                <input class="input-group-text"  style="width: 100%" type="number" name='ACS' value="">
+                <input class="input-group-text"  style="width: 100%" type="number" name='ACS' value="<?php $ACS; ?>">
                 </div>
+                    <?php validaACS(); ?>
                     <div class="col-8">
                 <select name="tipoACS" class="form-select input-group-text">>
                     <option value="Gas" <?php if(isset($_POST['enviar']) && ($_POST['calefaccion']=="Gas")) echo "selected"; ?>>Gas</option>
@@ -62,8 +73,9 @@ if (isset($_SESSION['user'])) {
                 <pre> kWh/m²                        Tipo</pre>
                 <div class="row mb-4">
                     <div class="col-4">
-                        <input class="input-group-text" style="width: 100%"  type="number" name="refrixeracion" value="">
-                 </div>
+                        <input class="input-group-text" style="width: 100%"  type="number" name="refrixeracion" value="<?php $refrixeracion; ?>">
+                    </div>
+                    <?php validaRef(); ?>
                     <div class="col-8">
                 <select name="tipoRef" class="form-select input-group-text">
                     <option value="Electricidade" <?php if(isset($_POST['enviar']) && ($_POST['calefaccion']=="Electricidade")) echo "selected"; ?>>Electricidade</option>
@@ -74,8 +86,9 @@ if (isset($_SESSION['user'])) {
                 <pre>Superficie                     Tipo</pre>
                 <div class="row mb-4">
                     <div class="col-4">
-                <input class="input-group-text" style="width: 100%" type="number" name="superficie" value="">
-                </div>
+                <input class="input-group-text" style="width: 100%" type="number" name="superficie" value="<?php $superficie; ?>">
+                    </div>
+                    <?php validaSup(); ?>
                     <div class="col-8">
                 <select name="tipoSuperf" class="form-select input-group-text">
                     <option value="Unifamiliar" <?php if(isset($_POST['enviar']) && ($_POST['superficie']=="unifamiliar")) echo "selected"; ?>>Unifamiliar</option>
@@ -115,13 +128,66 @@ if (isset($_SESSION['user'])) {
                          <div class="col-6 mt-5 ">                 
                 
         <?php
+         function validaCal(){
+                global $calefaccion;
+                global $datosOk;
+                if (isset($_POST['calcular'])) {
+                    // Validar que el titulo solo contenga letras
+                    if (Validacion::campoVacio($_POST['calefaccion'])) {
+                        Erro::addError('EmptyFieldErrorBody','O campo calefacción e requerido');
+                        $datosOk=false;
+                    } else {
+                        $calefaccion = $_POST['calefaccion'];
+                        
+                    }
+                }
+            }
+            
+            function validaACS(){
+                global $ACS;
+                global $datosOk;
+                if (isset($_POST['calcular'])) {
+                    // Validar que el titulo solo contenga letras
+                    if (Validacion::campoVacio($_POST['ACS'])) {
+                        Erro::addError('EmptyFieldErrorBody','O campo ACS e requerido');
+                        $datosOk=false;
+                    } else {
+                        $ACS = $_POST['ACS'];
+                    }
+                }
+            }
+            
+            function validaRef() {
+                global $refrixeracion;
+                global $datosOk;
+                if (isset($_POST['calcular'])) {
+                    // Validar que el titulo solo contenga letras
+                    if (Validacion::campoVacio($_POST['refrixeracion'])) {
+                        Erro::addError('EmptyFieldErrorBody','O campo refrixeración e requerido');
+                        $datosOk=false;
+                    } else {
+                        $refrixeracion = $_POST['refrixeracion'];
+                    }
+                }
+            }
+            function validaSup()
+            {
+                global $superficie;
+                global $datosOk;
+                if (isset($_POST['calcular'])) {
+                    // Validar que el titulo solo contenga letras
+                    if (Validacion::campoVacio($_POST['superficie'])) {
+                        Erro::addError('EmptyFieldErrorBody','O campo superficie e requerido');
+                        $datosOk=false;
+                    } else {
+                        $superficie = $_POST['superficie'];
+                    }
+                }
+            }
         
-        if(isset($_POST['calcular'])){
+        if(isset($_POST['calcular']) && $datosOk){
            //Creacion de variables
-            $calefaccion=(int)$_POST['calefaccion'];
-            $ACS=(int)$_POST['ACS'];
-            $refrixeracion=(int)$_POST['refrixeracion'];
-            $superficie=(int)$_POST['superficie'];
+
             $potenciaPunta=(float)$_POST['potencia'];
             $provincia=$_POST['provincia'];
             $tipoCalc=$_POST['tipoCalc'];
@@ -141,6 +207,9 @@ if (isset($_SESSION['user'])) {
             <?php $Vivienda->consumoTotal($consumoCalc,$consumoACS,$consumoRefrig);?>
             
             <?php
+            }
+            else {
+                echo Erro::showErrors();
             }
         
         ?>
